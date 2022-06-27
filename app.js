d7c3a85370f94f3b00552b5bfff68d6e25cfc04a
EvgeniyIlin <evgeniy@live.ru>
2022-06-27 07:52:13 +0500

    end_err

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-http.createServer((request,response)) => {
+http.createServer((request,response) => {
     console.log('server work');
     response.end('gooo');
 }).listen(3000);
