202b6f3506a2895e95a3273c7b027191d0045b74
EvgeniyIlin <evgeniy@live.ru>
2022-06-27 19:26:01 +0500

    step_up

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const http = require('http');
+const url = require('url');
 
 http.createServer((request,response) => {
     console.log('server work');
     response.end('gooo');
+    let urlRequest = url.parse(request.url, true);
+    // console.log(urlRequest);
+    console.log(urlRequest.query.test);
 }).listen(3000);
